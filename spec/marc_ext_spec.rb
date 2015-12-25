3221b624c488810b00dd32b2ea076eb46ca705d1
George Stavrou <iphone@icleversoft.com>
2015-12-25 23:45:25 +0200

    test for subfield

--- spec/marc_ext_spec.rb
+++ spec/marc_ext_spec.rb
@@ -1,16 +1,25 @@
 require 'spec_helper'
 
 describe 'marc_ext' do
+  context 'subfield' do
+    let(:subfield){MARC::Subfield.new('a', 'value')}
+    it 'returns an array' do
+      expect(subfield.to_json).to eq ['a', 'value']
+    end
+  end
   context 'datafield' do
     let(:datafield){MARC::DataField.new('100', '1', '2', ['a', 'Subfield a'], ['b', 'Subfield b'])}
-    it "return indicators as string" do
+    it "returns indicators as string" do
       expect(datafield.indicators_as_string).to eq '12'
     end
     
-    it "return subfields in array" do
+    it "returns subfields in array" do
       expect(datafield.subfields_as_arrays).to eq [['a', 'Subfield a'], ['b', 'Subfield b']]
     end
 
+    it "returns a json" do
+      expect(datafield.to_json).to eq ['12', ['a', 'Subfield a'], ['b', 'Subfield b']]
+    end
   end
 
 end
